8befe98421e46acb4058f47d784ac7e5ed95ee98
Nick Pittas <npittas@gmail.com>
2026-06-10 23:14:22 +0300

    Set default router models_max to 1, fix duplicate signal
    Single-GPU use case: default to 1 max loaded model. Also removes
    duplicate valueChanged connection.

--- qt_app/app/pages/run.py
+++ qt_app/app/pages/run.py
@@ -856,7 +856,7 @@ class RunPage(PageBase):
         max_row.addWidget(QLabel("Max loaded models:", self._router_panel))
         self._models_max_spin = QSpinBox(self._router_panel)
         self._models_max_spin.setRange(1, 32)
-        self._models_max_spin.setValue(4)
+        self._models_max_spin.setValue(1)
         self._models_max_spin.valueChanged.connect(self._update_command_preview)
         max_row.addWidget(self._models_max_spin)
         max_row.addStretch(1)
